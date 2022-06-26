ff647589ddf2b2f7a01c11b3bda29590635e6e80
Sayan Chowdhury <sayan.chowdhury.aiml25@heritageit.edu.in>
2022-06-26 09:53:44 +0530

    fixed contracts

--- contracts/contracts/DeNotes.sol
+++ contracts/contracts/DeNotes.sol
@@ -5,49 +5,75 @@ pragma solidity ^0.8.7;
 // import "@openzeppelin/contracts/access/Ownable.sol";
 // import "@openzeppelin/contracts/utils/Counters.sol";
 
+
 // 1.'listNote'
 // 2.'buyNote'
 
 contract DeNotes {
-    event Paid(address indexed _from, uint256 _value);
-    address payable public owner;
+    event Paid(address indexed _from, uint _value);
+    
     Seller[] public seller;
     Note[] public notes;
-    mapping(string => uint256) public priceofNote;
-    struct Seller {
+    mapping(string=>uint) public priceofNote;
+    struct Seller{
         string sellerName;
+
     }
-    struct Note {
+    struct Note{
         string name;
         string department;
         string college;
         string subject;
-        uint256 price;
+        uint price;
+        address seler;
+
+
     }
 
-    Seller public person = Seller({sellerName: "john"});
 
-    function listNote() external {}
+    Seller public person=Seller({sellerName:"john"});
 
-    function addNote(
+
+        function addNote(
         string memory _name,
         string memory _department,
         string memory _college,
         string memory _subject,
-        uint256 _price
-    ) external {
-        notes.push(Note(_name, _department, _college, _subject, _price));
-        priceofNote[_name] = _price;
+        uint _price,
+        address payable _seller
+        ) external{
+
+        _seller=payable(msg.sender);
+        notes.push(Note(_name,_department,_college,_subject,_price,_seller));
+        priceofNote[_name]=_price;
     }
 
-    function Buy(address payable _addr, uint256 _amount) external payable {
+
+
+
+    function listNote() external {
+      
+       
+    }
+    
+
+    constructor() payable{
+   
+    }
+
+
+
+    address payable _addr;
+    _addr=_seller;
+    function Buy(address payable _addr,uint _amount) payable external{
+            
         require(msg.value >= _amount);
         _addr.transfer(msg.value);
-
+    
         emit Paid(msg.sender, msg.value);
     }
+    function balance() public view returns (uint256){
+    return payable(address(msg.sender)).balance;
+     }
 
-    function balance() public view returns (uint256) {
-        return payable(address(msg.sender)).balance;
-    }
 }
